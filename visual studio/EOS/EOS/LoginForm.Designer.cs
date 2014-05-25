b31575b0909ff1e4f8f484accafc85127829ebbe
Matt Schwartz <schwamat@gmail.com>
2014-05-24 23:16:34 -0500

    Initial commit sort of

---
+++ visual studio/EOS/EOS/LoginForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace EOS
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.continueOfflineButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.rememberUserCheckBox = new System.Windows.Forms.CheckBox();
+            this.forgotPasswordLink = new System.Windows.Forms.LinkLabel();
+            this.newAccountButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(276, 125);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(276, 151);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // continueOfflineButton
+            // 
+            this.continueOfflineButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.continueOfflineButton.Location = new System.Drawing.Point(465, 298);
+            this.continueOfflineButton.Name = "continueOfflineButton";
+            this.continueOfflineButton.Size = new System.Drawing.Size(107, 23);
+            this.continueOfflineButton.TabIndex = 5;
+            this.continueOfflineButton.Text = "Continue offline";
+            this.continueOfflineButton.UseVisualStyleBackColor = true;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(276, 177);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(100, 23);
+            this.loginButton.TabIndex = 6;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.Location = new System.Drawing.Point(465, 327);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(107, 23);
+            this.exitButton.TabIndex = 9;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // rememberUserCheckBox
+            // 
+            this.rememberUserCheckBox.AutoSize = true;
+            this.rememberUserCheckBox.Location = new System.Drawing.Point(163, 181);
+            this.rememberUserCheckBox.Name = "rememberUserCheckBox";
+            this.rememberUserCheckBox.Size = new System.Drawing.Size(94, 17);
+            this.rememberUserCheckBox.TabIndex = 10;
+            this.rememberUserCheckBox.Text = "Remember me";
+            this.rememberUserCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // forgotPasswordLink
+            // 
+            this.forgotPasswordLink.AutoSize = true;
+            this.forgotPasswordLink.Location = new System.Drawing.Point(273, 212);
+            this.forgotPasswordLink.Name = "forgotPasswordLink";
+            this.forgotPasswordLink.Size = new System.Drawing.Size(91, 13);
+            this.forgotPasswordLink.TabIndex = 11;
+            this.forgotPasswordLink.TabStop = true;
+            this.forgotPasswordLink.Text = "Forgot password?";
+            // 
+            // newAccountButton
+            // 
+            this.newAccountButton.Location = new System.Drawing.Point(465, 269);
+            this.newAccountButton.Name = "newAccountButton";
+            this.newAccountButton.Size = new System.Drawing.Size(107, 23);
+            this.newAccountButton.TabIndex = 12;
+            this.newAccountButton.Text = "New Account";
+            this.newAccountButton.UseVisualStyleBackColor = true;
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.newAccountButton);
+            this.Controls.Add(this.forgotPasswordLink);
+            this.Controls.Add(this.rememberUserCheckBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.continueOfflineButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.MaximumSize = new System.Drawing.Size(600, 400);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "loginForm";
+            this.Text = "Eos | Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button continueOfflineButton;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.CheckBox rememberUserCheckBox;
+        private System.Windows.Forms.LinkLabel forgotPasswordLink;
+        private System.Windows.Forms.Button newAccountButton;
+    }
+}
+
