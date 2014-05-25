4019a566b175a69a7d88bfa02523835d04b4b8fa
Matt Schwartz <schwamat@gmail.com>
2014-05-25 15:38:46 -0500

    Rendering loop added

---
+++ visual studio/EOS/EOS/utility/TimeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOS.utility {
+    class TimeHelper {
+
+        public static readonly long MILLISECONDS_PER_FRAME = (int) ((1 / 60f) * 1000);
+
+        public static long Now() {
+            return Now(TimeSpan.TicksPerMillisecond);
+        }
+
+        public static long Now(long resolution) {
+            return DateTime.Now.Ticks / resolution;
+        }
+    }
+}
