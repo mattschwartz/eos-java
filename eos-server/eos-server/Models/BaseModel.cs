7315e24541068dee200121c618ed6ee4470d3abc
Matt Schwartz <schwamat@gmail.com>
2014-06-06 22:20:13 -0500

    Actually added the BaseModel this time..

---
+++ eos-server/eos-server/Models/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace eos.server.Models {
+    public class BaseModel {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public Int32 Id {
+            get;
+            set;
+        }
+    }
+}
