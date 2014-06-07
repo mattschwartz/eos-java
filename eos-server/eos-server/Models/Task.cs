ed310a1e585ae81ccabc55494a549fac8e1e5e54
AndrewMarkley <andrew-tx@satx.rr.com>
2014-06-06 22:40:39 -0500

    added task model

---
+++ eos-server/eos-server/Models/Task.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace eos_server.Models
+{
+    public class Task
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("xPos")]
+        public Int32 xPos { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("yPos")]
+        public Int32 yPos { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("name")]
+        public Int32 name { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("comments")]
+        public Int32 comments { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("color")]
+        public Int32 color { get; set; }
+
+
+    }
+}
