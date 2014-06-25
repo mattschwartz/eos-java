7711bf4e120ce48fddfcca3082a17da20da2113f
AndrewMarkley <ajm501028@gmail.com>
2014-06-25 03:07:38 -0500

    got mvc to run

--- eos/eos/Properties/AssemblyInfo.cs
+++ eos/eos/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("eos_server")]
+[assembly: AssemblyTitle("eos")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("eos_server")]
+[assembly: AssemblyProduct("eos")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
