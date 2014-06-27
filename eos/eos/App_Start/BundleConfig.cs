84ea2ef2c15980997ecde4c57f472f353176f3aa
AndrewMarkley <ajm501028@gmail.com>
2014-06-27 02:32:01 -0500

    it runs, but no css is rendering

--- eos/eos/App_Start/BundleConfig.cs
+++ eos/eos/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace eos.App_Start
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
--- eos/eos/App_Start/Startup.Auth.cs
+++ eos/eos/App_Start/Startup.Auth.cs
@@ -22,6 +22,16 @@ namespace eos
                 LoginPath = new PathString("/Account/Login")
             });
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider {
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });
+
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
